81992291bdb5e71ac17c815c9e6d242cc6a49538
lerchenflo <126778614+lerchenflo@users.noreply.github.com>
2025-08-09 13:51:46 +0200

    Initial commit

---
+++ composeApp/src/iosMain/kotlin/com/lerchenflo/schneaggchat/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.lerchenflo.schneaggchat
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/jvmMain/kotlin/com/lerchenflo/schneaggchat/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.lerchenflo.schneaggchat
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
