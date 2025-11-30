e9c146fc1ac82f3f3400303522eede34ab7afdac
Fabi <f.scheffknecht@gmail.com>
2025-11-30 11:08:49 +0100

    signup input fields intuitiver gmacht #72

--- composeApp/src/commonMain/kotlin/org/lerchenflo/schneaggchatv3mp/settings/presentation/Settings.kt
+++ composeApp/src/commonMain/kotlin/org/lerchenflo/schneaggchatv3mp/settings/presentation/Settings.kt
@@ -31,7 +31,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
