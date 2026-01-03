9052d322c640a862aec511eb77c5e1d9abeceffd
lerchenflo <126778614+lerchenflo@users.noreply.github.com>
2026-01-03 13:25:08 +0100

    implemented fix #131

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -182,7 +182,7 @@ android {
         applicationId = "org.lerchenflo.schneaggchatv3mp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 6
+        versionCode = 7
         versionName = "3.0-beta"
     }
     packaging {
