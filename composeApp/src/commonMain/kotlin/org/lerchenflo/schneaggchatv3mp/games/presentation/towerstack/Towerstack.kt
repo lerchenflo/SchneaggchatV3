afa9834c65934049c49c90fac0f428a0a2c797c7
flo <florian.lerchenmueller@gmail.com>
2026-01-22 11:00:53 +0100

    fix merge conflicts

--- composeApp/src/commonMain/kotlin/org/lerchenflo/schneaggchatv3mp/games/presentation/towerstack/Towerstack.kt
+++ composeApp/src/commonMain/kotlin/org/lerchenflo/schneaggchatv3mp/games/presentation/towerstack/Towerstack.kt
@@ -3,9 +3,21 @@ package org.lerchenflo.schneaggchatv3mp.games.presentation.towerstack
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -16,20 +28,16 @@ import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.koin.androidx.compose.koinViewModel
+import org.koin.compose.koinInject
 import org.lerchenflo.schneaggchatv3mp.sharedUi.core.ActivityTitle
-import org.lerchenflo.schneaggchatv3mp.games.presentation.towerstack.GameAction
-import org.lerchenflo.schneaggchatv3mp.games.presentation.towerstack.GameState
-import org.lerchenflo.schneaggchatv3mp.games.presentation.towerstack.Platform
 
 @Composable
 fun TowerStackScreen(
-    viewModel: TowerstackViewModel = koinViewModel(),
+    viewModel: TowerstackViewModel = koinInject(),
     onBackClick: () -> Unit = {}
 ) {
     val gameState by viewModel.gameState.collectAsState()
