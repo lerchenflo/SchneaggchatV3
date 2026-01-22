40f9ba80b0620f07d1b9cd865a620a6b777bb471
flo <florian.lerchenmueller@gmail.com>
2026-01-22 22:07:33 +0100

    Readme + libs.versions.toml

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -66,7 +66,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
