71e7814b7705bb9f87bb6ce5807f476573c600d2
flo <florian.lerchenmueller@gmail.com>
2026-01-25 16:22:57 +0100

    Appstart fix desktop

--- composeApp/src/jvmMain/kotlin/org/lerchenflo/schneaggchatv3mp/main.kt
+++ composeApp/src/jvmMain/kotlin/org/lerchenflo/schneaggchatv3mp/main.kt
@@ -2,51 +2,12 @@ package org.lerchenflo.schneaggchatv3mp
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.mmk.kmpnotifier.extensions.composeDesktopResourcesPath
-import com.mmk.kmpnotifier.notification.NotifierManager
-import com.mmk.kmpnotifier.notification.configuration.NotificationPlatformConfiguration
-import org.koin.core.context.startKoin
 import org.lerchenflo.schneaggchatv3mp.app.App
-import org.lerchenflo.schneaggchatv3mp.di.desktopAppDatabaseModule
-import org.lerchenflo.schneaggchatv3mp.di.desktopDataStoreModule
-import org.lerchenflo.schneaggchatv3mp.di.desktopHttpAuthModule
-import org.lerchenflo.schneaggchatv3mp.di.desktopHttpModule
-import org.lerchenflo.schneaggchatv3mp.di.desktopLanguageManagerModule
-import org.lerchenflo.schneaggchatv3mp.di.desktopPictureManagerModule
-import org.lerchenflo.schneaggchatv3mp.di.desktopShareUtilsModule
-import org.lerchenflo.schneaggchatv3mp.di.desktopVersionModule
-import org.lerchenflo.schneaggchatv3mp.di.sharedmodule
 import java.awt.Dimension
-import java.io.File
 
 fun main() = application {
 
-    startKoin {
-        modules(desktopAppDatabaseModule, sharedmodule)
-
-        modules(
-            desktopHttpModule,
-            desktopHttpAuthModule,
-            desktopDataStoreModule,
-            desktopVersionModule,
-            desktopPictureManagerModule,
-            desktopShareUtilsModule,
-            desktopLanguageManagerModule
-        )
-    }
-
-
-    NotifierManager.initialize(
-        NotificationPlatformConfiguration.Desktop(
-            showPushNotification = true,
-            notificationIconPath = composeDesktopResourcesPath() + File.separator + "schneaggchat_logo_v3.png"
-        )
-    )
-
-    // Initialize custom notification manager for encrypted payload processing
-    //NotificationManager.initialize()
-
-    //AppInitializer.onApplicationStart()
+    onAppStart()
 
 
     Window(
---
+++ composeApp/src/jvmMain/kotlin/org/lerchenflo/schneaggchatv3mp/onAppStart.kt
@@ -0,0 +1,48 @@
+package org.lerchenflo.schneaggchatv3mp
+
+import com.mmk.kmpnotifier.extensions.composeDesktopResourcesPath
+import com.mmk.kmpnotifier.notification.NotifierManager
+import com.mmk.kmpnotifier.notification.configuration.NotificationPlatformConfiguration
+import org.koin.core.context.startKoin
+import org.koin.mp.KoinPlatform
+import org.lerchenflo.schneaggchatv3mp.di.desktopAppDatabaseModule
+import org.lerchenflo.schneaggchatv3mp.di.desktopDataStoreModule
+import org.lerchenflo.schneaggchatv3mp.di.desktopHttpAuthModule
+import org.lerchenflo.schneaggchatv3mp.di.desktopHttpModule
+import org.lerchenflo.schneaggchatv3mp.di.desktopLanguageManagerModule
+import org.lerchenflo.schneaggchatv3mp.di.desktopPictureManagerModule
+import org.lerchenflo.schneaggchatv3mp.di.desktopShareUtilsModule
+import org.lerchenflo.schneaggchatv3mp.di.desktopVersionModule
+import org.lerchenflo.schneaggchatv3mp.di.sharedmodule
+import java.io.File
+
+fun onAppStart() {
+    if (KoinPlatform.getKoinOrNull() == null) {
+        startKoin {
+            modules(desktopAppDatabaseModule, sharedmodule)
+
+            modules(
+                desktopHttpModule,
+                desktopHttpAuthModule,
+                desktopDataStoreModule,
+                desktopVersionModule,
+                desktopPictureManagerModule,
+                desktopShareUtilsModule,
+                desktopLanguageManagerModule
+            )
+        }
+    }
+
+
+    NotifierManager.initialize(
+        NotificationPlatformConfiguration.Desktop(
+            showPushNotification = true,
+            notificationIconPath = composeDesktopResourcesPath() + File.separator + "schneaggchat_logo_v3.png"
+        )
+    )
+
+    // Initialize custom notification manager for encrypted payload processing
+    //NotificationManager.initialize()
+
+    //AppInitializer.onApplicationStart()
+}
