a7c2ceb4f46dcd160a58decc8e153e3c8b08cd7e
Fabi <f.scheffknecht@gmail.com>
2026-01-25 16:51:32 +0100

    splash icon fix #270

--- androidApp/src/main/kotlin/org/lerchenflo/androidApp/MainActivity.kt
+++ androidApp/src/main/kotlin/org/lerchenflo/androidApp/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.appupdate.AppUpdateOptions
 import com.google.android.play.core.install.model.AppUpdateType
@@ -20,10 +22,10 @@ import org.lerchenflo.schneaggchatv3mp.utilities.NotificationManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        installSplashScreen()
+        //enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
-
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         NotifierManager.onCreateOrOnNewIntent(this.intent)
 
