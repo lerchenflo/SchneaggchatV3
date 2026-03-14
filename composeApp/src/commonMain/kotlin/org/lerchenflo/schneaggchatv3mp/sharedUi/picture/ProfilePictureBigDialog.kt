0c5e36ec902fdc715fcab4b8a796cd8383dd145e
lerchenflo <126778614+lerchenflo@users.noreply.github.com>
2026-03-14 14:47:13 +0100

    Picture dialogs fix

--- composeApp/src/commonMain/kotlin/org/lerchenflo/schneaggchatv3mp/sharedUi/picture/ProfilePictureBigDialog.kt
+++ composeApp/src/commonMain/kotlin/org/lerchenflo/schneaggchatv3mp/sharedUi/picture/ProfilePictureBigDialog.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -31,7 +30,6 @@ import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import coil3.compose.AsyncImage
-import io.ktor.client.request.invoke
 
 // Zoagt es Profilbild groß a
 @Composable
