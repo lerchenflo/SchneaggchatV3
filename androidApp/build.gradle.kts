65ce14a6b034ce90991d32efd6abbca3f748f96f
lerchenflo <126778614+lerchenflo@users.noreply.github.com>
2026-05-12 20:51:00 +0200

    Red heart icon fix

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "org.lerchenflo.schneaggchatv3mp"
         minSdk = 28
         targetSdk = 37
-        versionCode = 19
+        versionCode = 20
         versionName = "3.0.10"
     }
     packaging {
@@ -35,8 +35,10 @@ android {
         }
     }
     buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
+
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
         }
 
 
