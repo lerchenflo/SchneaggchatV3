cca7c41aa3a6f802fd7af71d7a4a90519ca0a9e5
flo <florian.lerchenmueller@gmail.com>
2026-08-13 21:27:33 +0200

    Roundtrip updated

--- composeApp/src/commonMain/kotlin/org/lerchenflo/schneaggchatv3mp/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/lerchenflo/schneaggchatv3mp/app/App.kt
@@ -2,11 +2,9 @@ package org.lerchenflo.schneaggchatv3mp.app
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -23,7 +21,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -100,7 +97,6 @@ import org.lerchenflo.schneaggchatv3mp.sharedUi.clearFocusOnTap
 import org.lerchenflo.schneaggchatv3mp.sharedUi.core.AutoFadePopup
 import org.lerchenflo.schneaggchatv3mp.sharedUi.core.OfflineBar
 import org.lerchenflo.schneaggchatv3mp.sharedUi.core.SnackbarPopup
-import org.lerchenflo.schneaggchatv3mp.utilities.IncomingDataManager
 import org.lerchenflo.schneaggchatv3mp.utilities.LanguageService
 import org.lerchenflo.schneaggchatv3mp.utilities.SnackbarManager
 import org.lerchenflo.schneaggchatv3mp.utilities.UiText
@@ -214,15 +210,6 @@ fun App() {
             Route.Games.GamesSelector
         )
 
-
-
-        LaunchedEffect(Unit) {
-            while (true) {
-                delay(2000)
-
-                println("DEBUG: ROOTBACKSTACK: ${rootBackStack.toList()}")
-            }
-        }
 
         //Initialize navigator (TO navigate from viewmodels)
         val navigator = koinInject<Navigator>()
