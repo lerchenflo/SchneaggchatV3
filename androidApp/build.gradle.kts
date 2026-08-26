6e682752e36814445ae26df61eef2c58df2c2acb
flo <florian.lerchenmueller@gmail.com>
2026-08-26 13:39:30 +0200

    new version + stringres

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "org.lerchenflo.schneaggchatv3mp"
         minSdk = 28
         targetSdk = 37
-        versionCode = 31
+        versionCode = 32
         versionName = "3.0.16"
     }
     packaging {
