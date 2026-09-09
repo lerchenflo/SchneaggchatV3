40ec9dd743f27f5ddb115d4e7b085d7bf1657a2e
flo <florian.lerchenmueller@gmail.com>
2026-09-09 06:40:15 +0200

    logout enpoint called

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "org.lerchenflo.schneaggchatv3mp"
         minSdk = 28
         targetSdk = 37
-        versionCode = 34
+        versionCode = 35
         versionName = "3.0.18"
     }
     packaging {
